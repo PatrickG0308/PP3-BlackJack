1575ec04d30cab3d1f8d1b0eb22f2177c48a2988
PatrickG0308 <>
2023-08-02 11:18:43 +0000

    add main function with options menu

--- run.py
+++ run.py
@@ -24,3 +24,36 @@ def get_valid_name():
 if __name__ == "__main__":
     username = get_valid_name()
     print(f"Welcome to your game of BlackJack {username}!")
+
+# Menu options to view instructions or start the game
+
+
+def main():
+    while True:
+        print("Please choose an option:")
+        print("1. View Instructions")
+        print("2. Start Game")
+
+        choice = input("Enter the number of your choice: ")
+
+        if choice == "1":
+            clear_terminal()
+            print("Instructions: \n")
+            print("1. The player and dealer are both dealt two cards \n")
+            print("2. The player will get both cards face up and the dealer will have one card hidden. \n")
+            print("3. The goal of the game is to get as close to 21 without going over (bust) \n")
+            print("4. Player will be given the option to Hit or Stand during your turn \n")
+            print("5. Once you are satisfied with your score and stand then its the dealers turn.\n")
+            print("6. The dealer must hit and draw a card on 16 but stand on 17. \n")
+            print("7. If the dealer goes over 21 he loses and you win.\n")
+            print("8. If both yourself and the dealer do not exceed 21 then the closest to 21 wins \n")
+            print("9. Game can end in a tie if both scores are the same \n")
+            print("10. Scores are recorded and will be reported at the end of the game \n")
+
+        elif choice == "2":
+            clear_terminal()
+            play_blackjack()
+            break
+        else:
+            print("Invalid choice. Please enter a valid option (1 or 2).")
+
