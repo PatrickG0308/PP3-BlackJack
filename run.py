6d4652883d909481951dcc056ce0b08ae7c2dabf
PatrickG0308 <>
2023-08-02 16:48:30 +0000

    removed space between instructions

--- run.py
+++ run.py
@@ -38,24 +38,23 @@ def main():
 
         if choice == "1":
             clear_terminal()
-            print("Instructions: \n")
-            print("1. The player and dealer are both dealt two cards \n")
+            print("Instructions:")
+            print("1. The player and dealer are both dealt two cards")
             print("2. The player will get both cards face up and the"
-                  " dealer will have one card hidden.\n")
+                  " dealer will have one card hidden")
             print("3. The goal of the game is to get as close to 21"
-                  " without going over(bust) \n")
+                  " without going over(bust)")
             print("4. Player will be given the option to Hit or"
-                  " Stand during your turn \n")
+                  " Stand during your turn")
             print("5. Once you are satisfied with your score and"
-                  " stand then its the dealers turn.\n")
+                  " stand then its the dealers turn.")
             print("6. The dealer must hit and draw a card on"
-                  " 16 but stand on 17. \n")
-            print("7. If the dealer goes over 21 he loses and you win.\n")
+                  " 16 but stand on 17.")
+            print("7. If the dealer goes over 21 he loses and you win.")
             print("8. If both yourself and the dealer do not"
-                  " exceed 21 then the closest to 21 wins \n")
-            print("9. Game can end in a tie if both scores are the same \n")
-            print("10. Scores are recorded and will be reported"
-                  " at the end of the game \n")
+                  " exceed 21 then the closest to 21 wins")
+            print("9. Game can end in a tie")
+            print("10. Scores will be reported at the end of the game")
 
         elif choice == "2":
             clear_terminal()
